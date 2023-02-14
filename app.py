bc81626fd1a5458228e7cae9c23f1a126f4511e1
andres-gonzalez-13 <andrecksantiago@gmail.com>
2023-02-13 21:44:55 -0500

    Seleccion de palabra al azar y dibujos a cargar

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
@@ -13,10 +14,14 @@ mysql = MySQL(app)
 
 app.secret_key = 'mysecretkey'
 
+#palabra a construir
+palabraAdiv = ''
+palabraCons = ''
+
 #pagina inicio
 @app.route('/')
 def index():
-    flash('letra enviada')
+    #   flash('letra enviada')
     return render_template('index.html')
 
 #otra pagina
@@ -25,8 +30,9 @@ def juego():
     if request.method == 'POST':
         char = request.form['char']
         #print(char)
+        randWord = random.randint(100,119)
         cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM word')
+        cur.execute('SELECT * FROM word where idWord = ' + str(randWord))
 
         myresult = cur.fetchall()
 
@@ -34,7 +40,23 @@ def juego():
             #print(x)
             flash(x)
 
-        
+        return redirect(url_for('index'))
+
+@app.route('/palabra', methods=['POST'])
+def palabra():
+    if request.method == 'POST':
+        #char = request.form['char']
+        #print(char)
+        randWord = random.randint(100,119)
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM word where idWord = ' + str(randWord))
+
+        myresult = cur.fetchall()
+
+        for x in myresult:
+            #print(x)
+            flash(x[1])
+
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
