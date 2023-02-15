84d8ed4e8cccc330ed2c467b89716c1982805b01
andres-gonzalez-13 <andrecksantiago@gmail.com>
2023-02-14 23:40:57 -0500

    Version Casi funcional, condicion de victoria y derrota

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import random
+import random, os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
@@ -15,30 +15,51 @@ mysql = MySQL(app)
 app.secret_key = 'mysecretkey'
 
 #palabra a construir
-palabraAdiv = ''
-palabraCons = ''
+palabraAdiv = '-----'
+palabraCons = 'Tunja'
+fails = 0
+#imagn
+img = os.path.join('static')
 
 #pagina inicio
 @app.route('/')
 def index():
     #   flash('letra enviada')
-    return render_template('index.html')
+    file = os.path.join(img, 'ahorcado'+str(fails)+'.png')
+    return render_template('index.html', image=file)
 
 #otra pagina
 @app.route('/juego', methods=['POST'])
 def juego():
     if request.method == 'POST':
+        # vari globales
+        global fails, palabraAdiv
         char = request.form['char']
         #print(char)
-        randWord = random.randint(100,119)
-        cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM word where idWord = ' + str(randWord))
-
-        myresult = cur.fetchall()
-
-        for x in myresult:
-            #print(x)
-            flash(x)
+        flash(str(len(palabraAdiv)) + ' Letras')
+        flash(palabraAdiv)
+        
+        if len(char) != 1:
+            flash('una letra por intento por favor')
+        else:
+            #registro de letra y puntos
+            if char in palabraCons:
+                for i, c in enumerate(palabraCons):
+                    #flash(str(i) + c)
+                    if c == char:
+                        temp = list(palabraAdiv) 
+                        temp[i] = c
+                        palabraAdiv = "".join(temp)
+                flash(palabraAdiv)        
+            else:
+                if fails == 10:
+                    flash('FIN :(')
+                else:
+                    fails = fails+1
+                    flash('no es correcto')
+        
+        if '-' not in palabraAdiv:
+            flash('ganaste :)')
 
         return redirect(url_for('index'))
 
@@ -47,16 +68,30 @@ def palabra():
     if request.method == 'POST':
         #char = request.form['char']
         #print(char)
+        global palabraCons,palabraAdiv, fails
         randWord = random.randint(100,119)
         cur = mysql.connection.cursor()
         cur.execute('SELECT * FROM word where idWord = ' + str(randWord))
 
         myresult = cur.fetchall()
-
+        fails = 0
         for x in myresult:
             #print(x)
-            flash(x[1])
+            
+            palabraCons = x[1]
+            palabraAdiv = x[1]
+            #flash(x[1])
 
+        #crear palabra vacia
+        for i, c in enumerate(palabraCons):
+            #flash(str(i) + c)
+            temp = list(palabraAdiv) 
+            temp[i] = '-'
+            palabraAdiv = "".join(temp)
+                
+        #mostrar en pagina
+        flash(str(len(palabraAdiv)) + ' Letras')
+        flash(palabraAdiv)
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
